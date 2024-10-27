aa8395529726df285ba01821cdfb3e7ae05a2b97
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2024-10-27 12:39:45 -0400

    feat: Add per-application margin settings
    You can now specify a margin for the cursor lock for each application.
    Use this if the borders are slightly off and need to be manually
    adjusted.

---
+++ src/AutoCursorLock.App/Models/BorderDimensionsExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) James La Novara-Gsell. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace AutoCursorLock.App.Models;
+
+using AutoCursorLock.Native;
+using AutoCursorLock.Sdk.Models;
+
+/// <summary>
+/// Extensions for <see cref="BorderDimensions"/>.
+/// </summary>
+internal static class BorderDimensionsExtensions
+{
+    /// <summary>
+    /// Applies a margin to the border dimensions.
+    /// </summary>
+    /// <param name="dimensions">The dimensions to adjust.</param>
+    /// <param name="margin">The margin to apply.</param>
+    /// <returns>The adjusted dimensions.</returns>
+    public static BorderDimensions ApplyMargin(this BorderDimensions dimensions, AppLockMargin margin)
+    {
+        return new BorderDimensions(
+            dimensions.Top + margin.Top,
+            dimensions.Bottom - margin.Bottom,
+            dimensions.Left + margin.Left,
+            dimensions.Right - margin.Right
+        );
+    }
+}
